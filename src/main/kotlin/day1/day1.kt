5b8d79ac169766e71f41df51848d09834ca99869
moscicky <moscicki.maciej619@gmail.com>
2022-12-01 21:50:29 +0100

    add O(n) solution for day 1

--- src/main/kotlin/day1/day1.kt
+++ src/main/kotlin/day1/day1.kt
@@ -2,6 +2,7 @@ package day1
 
 import java.io.File
 import java.util.PriorityQueue
+import kotlin.random.Random
 
 fun addToHeap(queue: PriorityQueue<Int>, element: Int, heapSize: Int) {
     queue.add(element)
@@ -10,10 +11,9 @@ fun addToHeap(queue: PriorityQueue<Int>, element: Int, heapSize: Int) {
     }
 }
 
-
 // time: O(n log k)
 // space O(k)
-fun main() {
+fun heapSolution() {
     val path = "src/main/kotlin/day1/input2.txt"
     val k = 3
     var current = 0
@@ -35,4 +35,75 @@ fun main() {
     val result = queue.stream().reduce { a, b -> a + b }.get()
 
     println(result)
+}
+
+// average time: O(N)
+fun quickSelectSolution() {
+    val k = 3
+    val path = "src/main/kotlin/day1/input2.txt"
+    var current = 0
+    val counts = mutableListOf<Int>()
+
+    File(path).useLines {
+        for (line in it) {
+            if (line == "") {
+                counts.add(current)
+                current = 0
+            } else {
+                current += line.trim().toInt()
+            }
+        }
+    }
+
+    counts.add(current)
+
+    quickSelect(counts, 0, counts.size - 1, counts.size - k)
+
+    var result = 0
+    for (i in counts.size - k until counts.size) {
+        result += counts[i]
+    }
+
+    println(result)
+}
+
+fun swap(arr: MutableList<Int>, from: Int, to: Int) {
+    val tmp = arr[from]
+    arr[from] = arr[to]
+    arr[to] = tmp
+}
+
+fun partition(arr: MutableList<Int>, from: Int, to: Int): Int {
+    val pivot = arr[to]
+    var left = from
+    for (i in from until to) {
+        if (arr[i] < pivot) {
+            swap(arr, left, i)
+            left++
+        }
+    }
+
+    swap(arr, left, to)
+    return left
+}
+
+fun quickSelect(counts: MutableList<Int>, from: Int, to: Int, wantedIdx: Int): MutableList<Int> {
+    val pivotIdx = Random.nextInt(from, to + 1)
+
+    swap(counts, pivotIdx, to)
+
+    val partitionIdx = partition(counts, from, to)
+
+    return if (partitionIdx == wantedIdx) {
+        counts
+    } else if (partitionIdx < wantedIdx) {
+        quickSelect(counts, partitionIdx + 1, to, wantedIdx);
+    } else {
+        quickSelect(counts, from, partitionIdx - 1, wantedIdx)
+    }
+}
+
+
+fun main() {
+    quickSelectSolution()
 }
